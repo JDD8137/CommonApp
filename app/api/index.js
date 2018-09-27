68db5a9ede52186e3104b222684605740a2482e1
vishalvijay30 <sinjaybrothers@gmail.com>
2018-09-27 17:06:17 -0400

    added university search by name and location

---
+++ app/api/index.js
@@ -0,0 +1,28 @@
+import _ from "lodash";
+import universities from "./universities";
+
+export const contains = ({ name, acronym, location }, query) => {
+  if (name.toUpperCase().includes(query.toUpperCase())
+    || acronym.toUpperCase().includes(query.toUpperCase())
+    || location.toUpperCase().includes(query.toUpperCase())) {
+    return true;
+  }
+
+  return false;
+};
+
+export const getUniversities = (limit = 15, query = "") => {
+  return new Promise((resolve, reject) => {
+    if (query.length == 0) {
+      resolve(_.take(universities, limit));
+    } else {
+      const formattedQuery = query.toLowerCase();
+      const results = _.filter(users, university => {
+        return contains(university, formattedQuery);
+      });
+      resolve(_.take(results, limit));
+    }
+  });
+}
+
+export default getUniversities;
--- app/screens/Search.js
+++ app/screens/Search.js
@@ -1,31 +1,118 @@
 import React, { Component } from "react";
 import {
-  Button,
-  Container,
-  Content,
-  Footer,
-  InputGroup,
-  Input,
-  Icon,
-  Spinner,
-  Text,
   View,
-} from 'native-base'
-import { styles } from '../styles/styles'
-import { colorStyles, colorPalette } from "../styles/colorStyles"
-
+  Text,
+  FlatList,
+  ActivityIndicator,
+  SafeAreaView
+} from "react-native";
+import { List, ListItem, SearchBar } from "react-native-elements";
+import _ from "lodash";
+import { getUniversities, contains } from "../api/index";
 
 export default class Search extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      loading: false,
+      data: [],
+      error: null,
+      query: "",
+      fullData: []
+    };
   }
 
-  render() {
-    const { navigate } = this.props.navigation;
+  componentDidMount() {
+    this.makeRemoteRequest();
+  }
+
+  makeRemoteRequest = _.debounce(() => {
+    this.setState({ loading: true });
+
+    getUniversities(15, this.state.query)
+      .then(universities => {
+        this.setState({
+          loading: false,
+          data: universities,
+          fullData: universities
+        });
+      })
+      .catch(error => {
+        this.setState({ error, loading: false });
+      });
+  }, 250);
+
+  handleSearch = text => {
+    const formattedQuery = text.toLowerCase();
+    const data = _.filter(this.state.fullData, university => {
+      return contains(university, formattedQuery);
+    });
+    this.setState({ data, query: text }, () => this.makeRemoteRequest());
+  };
+
+  renderSeparator = () => {
     return (
-      <Text style={styles.TitleName}>
-          FirstName LastName
-      </Text>
+      <View
+        style={{
+          height: 1,
+          width: "86%",
+          backgroundColor: "#CED0CE",
+          marginLeft: "14%"
+        }}
+      />
+    );
+  };
+
+  renderHeader = () => {
+    return (
+      <SearchBar
+        placeholder="Search by Univerity Name, Location or Acronym"
+        lightTheme
+        round
+        onChangeText={this.handleSearch}
+      />
+    );
+  };
+
+  renderFooter = () => {
+    if (!this.state.loading) return null;
+
+    return (
+      <View
+        style={{
+          paddingVertical: 20,
+          borderTopWidth: 1,
+          borderColor: "#CED0CE"
+        }}
+      >
+        <ActivityIndicator animating size="large" />
+      </View>
+    );
+  };
+
+  render() {
+    return (
+      <SafeAreaView>
+        <List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0 }}>
+          <FlatList
+            data={this.state.data}
+            renderItem={({ item }) => (
+              <ListItem
+                roundAvatar
+                title={`${item.name}`}
+                subtitle={`${item.acronym} ${item.location}`}
+                // avatar={{ uri: item.picture.thumbnail }}
+                containerStyle={{ borderBottomWidth: 0 }}
+              />
+            )}
+            keyExtractor={item => item.name}
+            ItemSeparatorComponent={this.renderSeparator}
+            ListHeaderComponent={this.renderHeader}
+            ListFooterComponent={this.renderFooter}
+          />
+        </List>
+      </SafeAreaView>
     );
   }
 }
