b987d8c6863889599d988b64a27ff86ecf05494d
Daniel Becker <dbecker.fl@gmail.com>
2018-09-30 15:35:15 -0400

    Added android support maybe?

--- android/app/src/main/java/com/commonapp/MainApplication.java
+++ android/app/src/main/java/com/commonapp/MainApplication.java
@@ -10,10 +10,20 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.facebook.FacebookSdk;
+import com.facebook.CallbackManager;
+import com.facebook.appevents.AppEventsLogger;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -26,7 +36,8 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new FBSDKPackage(),
-            new RNFirebasePackage()
+            new RNFirebasePackage(),
+            new FBSDKPackage(mCallbackManager)
       );
     }
 
